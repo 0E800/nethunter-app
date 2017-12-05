f1de3bff6f3cc0a3909acd42dade1a502186ab66
aeroroland <aeroroland@dontemailme>
2017-12-04 20:33:32 -0500

    merged in latest master

--- src/com/offsec/nethunter/AppNavHomeActivity.java
+++ src/com/offsec/nethunter/AppNavHomeActivity.java
@@ -117,8 +117,7 @@ public class AppNavHomeActivity extends AppCompatActivity implements
         TextView buildInfo1 = navigationHeadView.findViewById(R.id.buildinfo1);
         TextView buildInfo2 = navigationHeadView.findViewById(R.id.buildinfo2);
         buildInfo1.setText(String.format("Version: %s (%s)", BuildConfig.VERSION_NAME, Build.TAGS));
-        String buildUser = "Kali";
-        buildInfo2.setText(String.format("Built by %s at %s", buildUser, buildTime));
+        buildInfo2.setText(String.format("Built by %s at %s", BuildConfig.BUILD_NAME, buildTime));
 
         if (navigationView != null) {
             setupDrawerContent(navigationView);
